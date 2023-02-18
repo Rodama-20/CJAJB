63c04832091ddb28c1c8432abd169d3791ea5f19
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-02-18 22:51:10 +0100

    remove logging

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -10,21 +10,18 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-        $out->writeln('Registering');
         $this->validate($request, [
             'name'                  => 'required|max:255',
             'email'                 => 'required|email|unique:users',
             'password'              => 'required|min:8',
             'password_confirmation' => 'required|same:password',
         ]);
-        $out->writeln('Validated');
+
         $user = User::create([
             'name'     => $request->name,
             'email'    => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        $out->writeln('Created');
 
         return response($user + [
             'token' => $user->createToken('API TOKEN')->plainTextToken,
@@ -37,11 +34,13 @@ class AuthController extends Controller
             'email'    => 'required|email',
             'password' => 'required|min:8',
         ]);
+
         if (!auth()->attempt($request->only('email', 'password'))) {
             return response([
                 'error' => 'Invalid credentials',
             ], 401);
         }
+
         $user = User::where('email', $request->email)->first();
         $token = $user->createToken('token')->plainTextToken;
 
