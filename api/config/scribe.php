1a03f7dd5237fe7aafb79a16bbed537ec0c7b9a8
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-02-23 20:25:14 +0100

    Apply fixes from StyleCI (#14)

--- api/config/scribe.php
+++ api/config/scribe.php
@@ -74,7 +74,7 @@ return [
                  */
                 'headers' => [
                     'Content-Type' => 'application/json',
-                    'Accept' => 'application/json',
+                    'Accept'       => 'application/json',
                 ],
 
                 /*
@@ -253,7 +253,7 @@ return [
     /*
      * Text to place in the "Introduction" section, right after the `description`. Markdown and HTML are supported.
      */
-    'intro_text' => <<<INTRO
+    'intro_text' => <<<'INTRO'
 This documentation aims to provide all the information you need to work with our API.
 
 <aside>As you scroll, you'll see code examples for working with the API in different programming languages in the dark area to the right (or as part of the content on mobile).
@@ -350,7 +350,7 @@ INTRO
      * Customize the "Last updated" value displayed in the docs by specifying tokens and formats.
      * Examples:
      * - {date:F j Y} => March 28, 2022
-     * - {git:short} => Short hash of the last Git commit
+     * - {git:short} => Short hash of the last Git commit.
      *
      * Available tokens are `{date:<format>}` and `{git:<format>}`.
      * The format you pass to `date` will be passed to PhP's `date()` function.
@@ -438,5 +438,5 @@ INTRO
      * Tell Scribe which connections should be transacted here.
      * If you only use one db connection, you can leave this as is.
      */
-    'database_connections_to_transact' => [config('database.default')]
+    'database_connections_to_transact' => [config('database.default')],
 ];
